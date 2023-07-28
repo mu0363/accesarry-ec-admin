bf7b1d4376d3dc97898f2d612b0d2127455f4da7
mu0363 <dde036@gmail.com>
2023-07-28 09:38:58 +0900

    add test button

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Hello</p>;
+  return (
+    <div>
+      <p>Hello</p>
+      <Button size="default" variant="outline">
+        SUBMIT
+      </Button>
+    </div>
+  );
 }
